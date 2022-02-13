4010681898d2201fbca84bb4808638d5f5d03688
Prud-homme Florent <prudhomme.flo@gmail.com>
2022-02-13 21:05:01 +0100

    Ticket CRUD

--- litreview/urls.py
+++ litreview/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
+from django.contrib.auth.views import LoginView, LogoutView
+#from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView, PasswordResetView, PasswordResetDoneView
+#from django.contrib.auth.views import PasswordResetCompleteView, PasswordResetConfirmView
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -24,6 +26,23 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 import authentication.views
 import reviewsapp.views
 
+# path('logout/', LogoutView.as_view(), name='logout'),
+
+# path('password/change/', PasswordChangeView.as_view(
+#         template_name='authentication/password_change_form.html'),
+#     name='password_change'),
+# path('password/change/done/', PasswordChangeDoneView.as_view(
+#         template_name='authentication/password_change_done.html'),
+#     name='password_change_done'),
+
+# path('password/reset/', PasswordResetView.as_view(),
+#     name='password_reset'),
+# path('password/reset/confirm/', PasswordResetConfirmView.as_view(),
+#     name='password_reset_confirm'),
+# path('password/reset/complete/', PasswordResetCompleteView.as_view(),
+#     name='password_reset_complete'),
+# path('password/reset/done', PasswordResetDoneView.as_view(),
+#     name='password_reset_done'),
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,16 +50,16 @@ urlpatterns = [
             template_name='authentication/login.html',
             redirect_authenticated_user=True),
         name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', authentication.views.UserLogoutView.as_view(), name='logout'),
     path('signup/', authentication.views.signup_page, name='signup'),
-    path('change-password/', PasswordChangeView.as_view(
-            template_name='authentication/password_change_form.html'),
-        name='password_change'),
-    path('change-password/done/', PasswordChangeDoneView.as_view(
-            template_name='authentication/password_change_done.html'),
-        name='password_change_done'),
+
+    
     path('home/', reviewsapp.views.home, name='home'),
-    path('ticket/create/', reviewsapp.views.ticket_create, name='ticket_create'),
+
+    path('ticket/create/', reviewsapp.views.ticket_create, name='ticket-create'),
+    path('ticket/<int:ticket_id>/', reviewsapp.views.ticket_details, name='ticket-details'),
+    path('ticket/<int:ticket_id>/update/', reviewsapp.views.ticket_update, name='ticket-update'),
+    path('ticket/<int:ticket_id>/delete/', reviewsapp.views.ticket_delete, name='ticket-delete'),
 ]
 
 if settings.DEBUG:
--- reviewsapp/views.py
+++ reviewsapp/views.py
@@ -1,7 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+from django.contrib import messages
 
+from . import forms, models
 
 @login_required
 def home(request):
-    return render(request, 'reviewsapp/home.html')
+    tickets = models.Ticket.objects.all()
+    return render(request, 'reviewsapp/home.html', context={'tickets': tickets})
+
+@login_required
+def ticket_create(request):
+    form = forms.TicketForm()
+    if request.method == 'POST':
+        form = forms.TicketForm(request.POST, request.FILES)
+        if form.is_valid():
+
+            ticket = form.save(commit=False)
+            ticket.user = request.user
+            ticket.save()
+            return redirect('home')
+    return render(request, 'reviewsapp/ticket_create.html', context={'form': form})
+
+@login_required
+def ticket_details(request, ticket_id):
+    ticket = get_object_or_404(models.Ticket, id=ticket_id)
+    return render(request, 'reviewsapp/ticket_details.html', {'ticket': ticket})
+
+@login_required
+def ticket_update(request, ticket_id):
+    ticket = models.Ticket.objects.get(id=ticket_id)
+
+    if request.method == 'POST':
+        form = forms.TicketForm(request.POST, request.FILES, instance=ticket)
+        if form.is_valid():
+            # mettre à jour le groupe existant dans la base de données
+            form.save()
+            # rediriger vers la page détaillée du groupe que nous venons de mettre à jour
+            return redirect('ticket-details', ticket.id)
+    else:
+        form = forms.TicketForm(instance=ticket)
+
+    return render(request,
+                'reviewsapp/ticket_update.html',
+                {'form': form})
+
+@login_required
+def ticket_delete(request, ticket_id):
+    ticket = models.Ticket.objects.get(id=ticket_id)
+    if request.method == 'POST':
+        # supprimer le groupe de la base de données
+        #ticket.delete()
+        # rediriger vers la liste des groupes
+        messages.success(request, "Ticket has been deleted" )
+        return redirect('home')
+
+    return render(request,
+           'reviewsapp/ticket_delete.html',
+           {'ticket': ticket})
